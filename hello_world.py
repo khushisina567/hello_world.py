16fbeab5d60ec4e3b42b2d7f3d7fd46215033bf3
khushisina567 <khushisinha12a2021@gmail.com>
2026-01-15 15:34:10 +0530

    Added Task 1 files

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+from datetime import date
+today_date = date.today()
+name = input("Enter your name: ")
+role = input("Enter your internship role: ")
+print("\n----- Internship Details -----")
+print("Name:", name)
+print("Internship Role:", role)
+print("Today's Date:", today_date)
